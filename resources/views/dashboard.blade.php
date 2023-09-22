9a3a60dd3e096e0071735ccadb47d675310ecfa7
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-09-22 13:23:43 +0700

    menambahkan alert pada setiap user
    by habibie

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if ($message = Session::get('message'))
+                    <h1>{{$message}}</h1>
+                    @endif
                 </div>
             </div>
         </div>
