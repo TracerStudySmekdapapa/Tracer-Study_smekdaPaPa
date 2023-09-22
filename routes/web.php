982f70c2cf9bad3609e5053e80aa614a4dee1eae
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-09-22 13:22:17 +0700

    menambahkan route
    by habibie

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/authenticate', [AuthenticateController::class, 'index'])->middleware(['auth', 'verified'])->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
