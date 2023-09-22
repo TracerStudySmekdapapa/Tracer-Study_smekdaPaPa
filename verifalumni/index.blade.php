b3f9c946f96443155a46000e410d41e6b0c9cf21
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-09-22 19:36:04 +0700

    menambahkan folder view alumni
    habibie

---
+++ verifalumni/index.blade.php
@@ -0,0 +1,43 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table class="table-fixed">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($user as $data)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+                                    <td>{{ $data->name }}</td>
+                                    <td>{{ $data->email }}</td>
+                                    <td>
+                                        <form action="{{ route('verifalumniStore', $data->id_user) }}" method="POST">
+                                            @csrf
+
+                                            <a href="">tolak</a>
+                                            <button type="submit">veriaf</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
