64fcc514de27a377feb089bf04cbfc26385580d2
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-09-22 13:26:28 +0700

    mengubah migration table user
    by habibie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_user');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('profil_picture')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
