c662eb428b85eec73653a119fb48c11683061949
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-09-22 13:29:16 +0700

    mengubah model user
    by habibie

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    protected $primaryKey = 'id_user';
     /**
      * The attributes that are mass assignable.
      *
