2fa3ab72ab4851e25c85e93bb755089ccc696834
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-10-09 14:14:26 +0700

    merubah name
    merubah name agar bisa data tersimpan tidak null name sebelumnya salah

--- resources/views/alumni/datapendidikan/create.blade.php
+++ resources/views/alumni/datapendidikan/create.blade.php
@@ -64,7 +64,7 @@
                     <!-- nama_pekerjaan -->
                     <label class="block text-sm">
                         <span class="text-gray-700 dark:text-gray-400">Nama Universitas</span>
-                        <input type="text" name="nama_universitas"
+                        <input type="text" name="nama_univ"
                             class="block w-full  mt-1 text-sm border border-gray-600 px-5 py-2 rounded-md dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                             placeholder="nama univ" />
                     </label>
