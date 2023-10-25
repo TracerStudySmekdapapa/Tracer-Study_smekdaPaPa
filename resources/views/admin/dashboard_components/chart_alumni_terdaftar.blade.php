59b4eb5e161547c846d290a65ddbd6694ae590c6
TracerStudySmekdapapa <osisakun029@gmail.com>
2023-10-25 16:27:45 +0700

    perubahab  library dashboard
    by syaid

--- resources/views/admin/dashboard_components/chart_alumni_terdaftar.blade.php
+++ resources/views/admin/dashboard_components/chart_alumni_terdaftar.blade.php
@@ -1,37 +1,39 @@
-<canvas id="myChart" style="width:100%;max-width:480"></canvas>
+<canvas id="chart" style="width:100%;max-width:480"></canvas>
 
 <script>
-    const xValues = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-        2022, 2023, 2024, 2025, 2026
-    ];
-    const yValues = [190, 200, 230, 300, 204, 300, 240, 334, 232, 251, 123, 221, 142, 223, 209, 309, 203, 223, 200, 100,
-        50
-    ];
-
-    new Chart("myChart", {
-        type: "line",
-        data: {
-            labels: xValues,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                backgroundColor: "rgba(0,0,255,1.0)",
-                borderColor: "rgba(0,0,255,0.1)",
-                data: yValues
-            }]
-        },
-        options: {
-            legend: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 0,
-                        max: 400
-                    }
-                }],
+    var options = {
+        series: [{
+            name: "Desktops",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }],
+        chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+                enabled: false
             }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'straight'
+        },
+        title: {
+            text: 'Product Trends by Month',
+            align: 'left'
+        },
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+            },
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
         }
-    });
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 </script>
