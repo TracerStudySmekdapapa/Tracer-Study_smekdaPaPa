4125edbe700eee6b2a68413bacd7783b98e6d07c
TracerStudySmekdapapa <osisakun029@gmail.com>
2023-11-02 15:37:27 +0700

    update isi email reset password
    Update dan ganti bahasa ke indonesia isi email change password oleh Kevin

--- vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
+++ vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
@@ -1,52 +1,58 @@
 <x-mail::message>
-    {{-- Greeting --}}
-    @if (!empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # @lang('Whoops!')
-        @else
-            # @lang('Halo!')
-        @endif
-    @endif
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('Halo!')
+@endif
+@endif
 
-    {{-- Intro Lines --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
 
-    {{-- Action Button --}}
-    @isset($actionText)
-        <?php
-        $color = match ($level) {
-            'success', 'error' => $level,
-            default => 'primary',
-        };
-        ?>
-        <x-mail::button :url="$actionUrl" :color="$color">
-            {{ $actionText }}
-        </x-mail::button>
-    @endisset
+@endforeach
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    $color = match ($level) {
+        'success', 'error' => $level,
+        default => 'primary',
+    };
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+{{ $actionText }}
+</x-mail::button>
+@endisset
 
-    {{-- Salutation --}}
-    @if (!empty($salutation))
-        {{ $salutation }}
-    @else
-        @lang('TracerStudy'),<br>
-        {{ config('app.name') }}
-    @endif
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
 
-    {{-- Subcopy --}}
-    @isset($actionText)
-        <x-slot:subcopy>
-            @lang("Jika Anda kesulitan mengklik \":actionText\" button, salin dan tempel URL di bawah\n" . 'into your web browser:', [
-                'actionText' => $actionText,
-            ]) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-        </x-slot:subcopy>
-    @endisset
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('TracerStudy'),<br>
+{{ config('app.name') }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+<x-slot:subcopy>
+@lang(
+    "Jika Anda kesulitan mengklik \":actionText\" button, salin dan tempel URL di bawah\n".
+    'ke web browser anda:',
+    [
+        'actionText' => $actionText,
+    ]
+) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+</x-slot:subcopy>
+@endisset
 </x-mail::message>
