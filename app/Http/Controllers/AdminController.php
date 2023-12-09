b80938e0404a5a14845e760a3e9ef0691f394bd8
TracerStudySmekdapapa <145521617+TracerStudySmekdapapa@users.noreply.github.com>
2023-12-09 09:28:42 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -205,21 +205,21 @@ class AdminController extends Controller
         return view('admin.users.index', compact('tidakAlumni', 'title', 'title_page', 'tolakAlumni', 'data', 'dataCount'));
     }
 
-    public function editUsers($id)  {
+    public function editUsers($id)
+    {
         $title = 'Edit Data Users';
         $title_page = 'Edit Data Users';
         $tidakAlumni = User::tidakAlumni()->limit(3)->get();
         $tolakAlumni = User::tolakAlumni()->limit(3)->get();
         $data = User::find($id);
-        $roles = Role::pluck('name','name')->all();
-        $userRole = $data->roles->pluck('name','name')->all();
+        $roles = Role::pluck('name', 'name')->all();
+        $userRole = $data->roles->pluck('name', 'name')->all();
 
         return view('admin.users.edit', compact('title', 'title_page', 'data', 'tidakAlumni', 'tolakAlumni', 'roles', 'userRole'));
-        
     }
 
-    public function updateUsers(Request $request, $id_user)  {
-        $user = User::find($id_user);
+    public function updateUsers(Request $request, $id_user)
+    {
         $data = User::where('id_user', $id_user)->first();
 
         $validatedData = $request->validate([
@@ -234,7 +234,7 @@ class AdminController extends Controller
                 'email' => $request->email,
             ]);
 
-            if ($request->filled('password')){
+            if ($request->filled('password')) {
                 // Encrypt new password
                 $data->password = Hash::make($request->password);
                 $data->save();
@@ -246,7 +246,8 @@ class AdminController extends Controller
         }
     }
 
-    public function destroyUsers($id_user)  {
+    public function destroyUsers($id_user)
+    {
         $user = User::where('id_user', $id_user)->first();
         $user->delete();
 
