c11ef845d5a8e98f82655c8950fc7682715e20fc
TracerStudySmekdapapa <osisakun029@gmail.com>
2023-12-12 16:29:21 +0700

    delete file
    by syaid

--- vendor/recca0120/terminal/resources/ts/commands/composer.ts
+++
@@ -1,9 +0,0 @@
-import {Command} from '../command';
-
-export class Composer extends Command {
-    protected commandLine: boolean = true;
-
-    is(command: string): boolean {
-        return /^(\.\/)?composer/.test(command);
-    }
-}
